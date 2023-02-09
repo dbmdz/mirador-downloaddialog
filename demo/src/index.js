3ecc4dc88633802f5f17fd86c9006acbc416115e
Matthias Lindinger <matthias.lindinger@bsb-muenchen.de>
2023-02-09 11:25:19 +0100

    Add plugin stub

---
+++ demo/src/index.js
@@ -0,0 +1,37 @@
+import Mirador from "mirador/dist/es/src/index";
+
+import downloadDialogPlugin from "../../src";
+
+const config = {
+  catalog: [
+    {
+      manifestId:
+        "https://api.digitale-sammlungen.de/iiif/presentation/v2/bsb00135902/manifest",
+      provider: "Bavarian State Library",
+    },
+    {
+      manifestId:
+        "https://api.digitale-sammlungen.de/iiif/presentation/v2/bsb10532463_00005_u001/manifest",
+      provider: "Bavarian State Library",
+    },
+    {
+      manifestId:
+        "https://api.digitale-sammlungen.de/iiif/presentation/v2/bsb00034024/manifest",
+      provider: "Bavarian State Library",
+    },
+  ],
+  id: "demo",
+  window: {
+    allowFullscreen: true,
+  },
+  windows: [
+    {
+      canvasIndex: 8,
+      manifestId:
+        "https://api.digitale-sammlungen.de/iiif/presentation/v2/bsb00034024/manifest",
+      view: "single",
+    },
+  ],
+};
+
+Mirador.viewer(config, [...downloadDialogPlugin]);
