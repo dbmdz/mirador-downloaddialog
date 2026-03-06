3c2e1d8002ae66b0496cfe8256b75caa2ccd30df
Stefan Schweter <stefan@schweter.it>
2026-03-06 17:22:51 +0100

    feat: add vite config

---
+++ vite.config.js
@@ -0,0 +1,43 @@
+import react from "@vitejs/plugin-react-swc";
+import { resolve } from "path";
+import { defineConfig } from "vite";
+
+const buildMode = process.env.BUILD_MODE ?? "plugin";
+
+const pluginConfig = {
+  build: {
+    copyPublicDir: false,
+    lib: {
+      entry: resolve(__dirname, "src/index.js"),
+      formats: ["es"],
+    },
+    rollupOptions: {
+      external: [
+        /^@emotion\/(react|styled)/,
+        /^@mui\/(material|icons-material|system)/,
+        "mirador",
+        "react",
+        "react-dom",
+        "react-i18next",
+      ],
+      output: {
+        entryFileNames: "[name].js",
+      },
+    },
+  },
+  plugins: [react()],
+};
+
+const demoConfig = {
+  build: {
+    outDir: resolve(__dirname, "demo/dist"),
+    rollupOptions: {
+      input: {
+        demo: resolve(__dirname, "index.html"),
+      },
+    },
+  },
+  plugins: [react()],
+};
+
+export default defineConfig(buildMode !== "demo" ? pluginConfig : demoConfig);
