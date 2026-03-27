d5957e60dc0804f30f248c841337b1d6c90ee37e
Stefan Schweter <stefan@schweter.it>
2026-03-27 12:21:08 +0100

    feat: make use of reacts's component type in download dialog plugin area container

--- src/containers/dialog/DownloadDialogPluginArea.ts
+++ src/containers/dialog/DownloadDialogPluginArea.ts
@@ -1,8 +1,11 @@
 import { withPlugins } from "mirador";
+import { ComponentType } from "react";
 import { withTranslation } from "react-i18next";
 
 import DownloadDialogPluginArea from "../../components/dialog/DownloadDialogPluginArea";
 
+type Props = { windowId: string };
+
 export default withTranslation()(
   withPlugins("DownloadDialogPluginArea")(DownloadDialogPluginArea),
-);
+) as ComponentType<Props>;
