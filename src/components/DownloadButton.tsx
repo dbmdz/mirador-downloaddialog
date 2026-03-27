115ed33dd0becbd051b423fa3af0a01e532cd68b
Stefan Schweter <stefan@schweter.it>
2026-03-27 12:18:32 +0100

    feat: remove prop-types from download button component and write own props

--- src/components/DownloadButton.tsx
+++ src/components/DownloadButton.tsx
@@ -1,8 +1,22 @@
 import DownloadIcon from "@mui/icons-material/VerticalAlignBottomSharp";
 import { MiradorMenuButton } from "mirador";
-import PropTypes from "prop-types";
+import { TFunction } from "i18next";
 
-const DownloadButton = ({ config, containerId, t, updateConfig }) => {
+import { PluginConfig } from "../state/selectors";
+
+type DownloadButtonProps = {
+  config: PluginConfig;
+  containerId: string;
+  t: TFunction;
+  updateConfig: (config: PluginConfig) => void;
+};
+
+const DownloadButton = ({
+  config,
+  containerId,
+  t,
+  updateConfig,
+}: DownloadButtonProps) => {
   const { dialogOpen, enabled } = config;
   if (!enabled) {
     return null;
@@ -23,16 +37,6 @@ const DownloadButton = ({ config, containerId, t, updateConfig }) => {
       <DownloadIcon />
     </MiradorMenuButton>
   );
-};
-
-DownloadButton.propTypes = {
-  config: PropTypes.shape({
-    dialogOpen: PropTypes.bool.isRequired,
-    enabled: PropTypes.bool.isRequired,
-  }).isRequired,
-  containerId: PropTypes.string.isRequired,
-  t: PropTypes.func.isRequired,
-  updateConfig: PropTypes.func.isRequired,
 };
 
 export default DownloadButton;
